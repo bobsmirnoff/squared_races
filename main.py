2511993b39afbfed65280992fd8fb0ae1884e1ee
Boris Smirnov <boris.v.smirnov@gmail.com>
2021-08-27 01:11:37 +0300

    map parsing improved

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ class Game:
         self.player = Player(self, RED)
         # convert map data to curb tiles
         for row, tiles in enumerate(self.map_data):
+            print(row, tiles[:-1])
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     CurbTile(self, col, row)
